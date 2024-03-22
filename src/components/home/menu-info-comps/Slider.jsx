e3a03694e6d09992ee431ae7b754a6af4639112d
DizzlyD <david4henery00@hotmail.com>
2024-03-22 21:05:36 +0000

    tabley-home-layout

--- src/components/home/menu-info-comps/Slider.jsx
+++ src/components/home/menu-info-comps/Slider.jsx
@@ -8,19 +8,19 @@ const Slider = () => {
       <div className="h-full col-start-1 col-end-13 row-start-1 row-end-13">
         <Image
           src="/images/home-menu-slider/steak-background.png"
-          width={1020}
-          height={1020}
+          width={1620}
+          height={1620}
           alt="large cutting board with multiple slices of neatly cut, rare steak"
           className="h-full object-cover object-center"
         />
       </div>
-      <div className="col-start-2 col-end-12 row-start-12 row-end-13 mt-4 mb-16 flex items-center justify-center gap-2 lgMob:mb-24">
-        <div className="w-6 h-6 rounded-full bg-red border-4 border-black"/>
-        <div className="w-6 h-6 rounded-full bg-white border-4 border-black"/>
-        <div className="w-6 h-6 rounded-full bg-white border-4 border-black"/>
-        <div className="w-6 h-6 rounded-full bg-white border-4 border-black"/>
-        <div className="w-6 h-6 rounded-full bg-white border-4 border-black"/>
-        <div className="w-6 h-6 rounded-full bg-white border-4 border-black"/>
+      <div className="col-start-2 col-end-12 row-start-12 row-end-13 max-w-[350px] mx-auto mt-4 mb-16 flex items-center justify-center gap-2 lgMob:mb-24 smTab:mx-0">
+        <div className="w-6 h-6 rounded-full bg-red border-4 border-black" />
+        <div className="w-6 h-6 rounded-full bg-white border-4 border-black" />
+        <div className="w-6 h-6 rounded-full bg-white border-4 border-black" />
+        <div className="w-6 h-6 rounded-full bg-white border-4 border-black" />
+        <div className="w-6 h-6 rounded-full bg-white border-4 border-black" />
+        <div className="w-6 h-6 rounded-full bg-white border-4 border-black" />
       </div>
     </>
   );
